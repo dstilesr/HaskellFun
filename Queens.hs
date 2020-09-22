bbd4e002656d73e4e2eabf34c0078acae168dd0e
dstilesr <d.stiles.r@hotmail.com>
2020-09-21 20:59:14 -0500

    Added solution to NQueens problem

---
+++ Queens.hs
@@ -0,0 +1,37 @@
+
+
+type Queen = (Int, Int)
+
+queensAttack :: Queen -> Queen -> Bool
+queensAttack (p1, p2) (q1, q2) = or [p1 == q1,
+                                     p2 == q2,
+                                     (abs (p1 - q1)) == (abs (p2 - q2))]
+
+
+newQueen :: [Queen] -> Queen -> Bool
+newQueen qs q = not $ or [queensAttack p q | p <- qs]
+
+
+numsTo :: Int -> [Int]
+numsTo n
+      | n > 0     = [0..(n - 1)]
+      | otherwise = []
+
+
+countSol :: Int -> [Queen] -> Int
+countSol 0 _ = 0
+countSol n qs
+           | (length qs) == (n - 1) = if or [newQueen qs (n - 1, i)| 
+                                            i <- (numsTo n)]
+                                         then 1 else 0
+           | otherwise              = sum [countSol n $ (length qs, i):qs | 
+                                           i <- (numsTo n),
+                                           newQueen qs (length qs, i)]
+
+
+main :: IO ()
+main = do putStrLn "Enter Number of Queens:"
+          num <- getLine
+          n <- return $ read num
+          putStrLn . show $ countSol n []
+
